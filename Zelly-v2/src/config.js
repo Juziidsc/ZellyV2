765643b7861446ca25b989c3cd56a54dba9a24c7
Juziidsc <156546284+Juziidsc@users.noreply.github.com>
2024-01-13 15:47:58 -0500

    Add files via upload

---
+++ Zelly-v2/src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    token: 'MTE5NTUyNjg5NzcyODIzNzYwOQ.G35HKF.hx9hfuJdJ6bDLKOEs85HV07F6oJAG9ATkUnwOE',
+    devID: ['910765858178088970'],
+    applicationId: '1195526897728237609'
+};
